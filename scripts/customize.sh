0b9f8ad598a125a0065442ad32b65913cf4541c0
M Aswin Kishore <60577077+mak626@users.noreply.github.com>
2023-03-04 11:49:21 +0530

    revert: magisk scripts

--- scripts/customize.sh
+++ scripts/customize.sh
@@ -1,12 +1,10 @@
-# shellcheck disable=SC2148,SC2086
+# shellcheck disable=SC2148,SC2086,SC2115
 ui_print ""
 
 if [ $ARCH = "arm" ]; then
-	#arm
-	ARCH_LIB=armeabi-v7a
 	alias cmpr='$MODPATH/bin/arm/cmpr'
+	ARCH_LIB=armeabi-v7a
 elif [ $ARCH = "arm64" ]; then
-	#arm64
 	ARCH_LIB=arm64-v8a
 	alias cmpr='$MODPATH/bin/arm64/cmpr'
 else
@@ -14,56 +12,37 @@ else
 fi
 set_perm_recursive $MODPATH/bin 0 0 0755 0777
 
-su -Mc grep __PKGNAME /proc/mounts | while read -r line; do
+basepath() {
+	basepath=$(pm path __PKGNAME | grep base)
+	echo ${basepath#*:}
+}
+
+grep __PKGNAME /proc/self/mountinfo | while read -r line; do
 	ui_print "* Un-mount"
-	mp=${line#* }
-	mp=${mp%% *}
-	su -Mc umount -l ${mp%%\\*}
+	mountpoint=$(echo "$line" | cut -d' ' -f5)
+	umount -l "${mountpoint%%\\*}"
 done
 am force-stop __PKGNAME
 
-INS=true
-if BASEPATH=$(pm path __PKGNAME); then
-	BASEPATH=${BASEPATH##*:}
-	BASEPATH=${BASEPATH%/*}
-	if [ ! -d ${BASEPATH}/lib ]; then
-		ui_print "* Invalid installation found. Uninstalling..."
-		pm uninstall -k --user 0 __PKGNAME
-	elif cmpr $BASEPATH/base.apk $MODPATH/__PKGNAME.apk; then
-		ui_print "* __PKGNAME is up-to-date"
-		INS=false
-	fi
-fi
-if [ $INS = true ]; then
+BASEPATH=$(basepath)
+if [ -n "$BASEPATH" ] && cmpr $BASEPATH $MODPATH/__PKGNAME.apk; then
+	ui_print "* Updating with stock APK is not needed"
+else
 	ui_print "* Updating __PKGNAME (v__PKGVER)"
-	SZ=$(stat -c "%s" $MODPATH/__PKGNAME.apk)
-	if ! SES=$(pm install-create --user 0 -i com.android.vending -r -d -S "$SZ" 2>&1); then
-		ui_print "ERROR: session creation failed"
-		abort "$SES"
-	fi
-	SES=${SES#*[}
-	SES=${SES%]*}
-	set_perm "$MODPATH/__PKGNAME.apk" 1000 1000 644 u:object_r:apk_data_file:s0
-	if ! op=$(pm install-write -S "$SZ" "$SES" "__PKGNAME.apk" "$MODPATH/__PKGNAME.apk" 2>&1); then
-		ui_print "ERROR: install-write failed"
+	set_perm $MODPATH/__PKGNAME.apk 1000 1000 644 u:object_r:apk_data_file:s0
+	if ! op=$(cmd package install --user 0 -i com.android.vending -r -d $MODPATH/__PKGNAME.apk 2>&1); then
+		ui_print "ERROR: APK installation failed!"
 		abort "$op"
 	fi
-	if ! op=$(pm install-commit "$SES" 2>&1); then
-		ui_print "ERROR: install-commit failed"
-		abort "$op"
-	fi
-	if BASEPATH=$(pm path __PKGNAME); then
-		BASEPATH=${BASEPATH##*:}
-		BASEPATH=${BASEPATH%/*}
-	else
+	BASEPATH=$(basepath)
+	if [ -z "$BASEPATH" ]; then
 		abort "ERROR: install __PKGNAME manually and reflash the module"
 	fi
 fi
-BASEPATHLIB=${BASEPATH}/lib/${ARCH}
-if [ -z "$(ls -A1 ${BASEPATHLIB})" ]; then
+BASEPATHLIB=${BASEPATH%base.apk}lib/${ARCH}
+if ! ls ${BASEPATHLIB}/*.so; then
 	ui_print "* Extracting native libs"
-	mkdir -p $BASEPATHLIB
-	if ! op=$(unzip -j $MODPATH/__EXTRCT lib/${ARCH_LIB}/* -d ${BASEPATHLIB} 2>&1); then
+	if ! op=$(unzip -j $MODPATH/__PKGNAME.apk lib/${ARCH_LIB}/* -d ${BASEPATHLIB} 2>&1); then
 		ui_print "ERROR: extracting native libs failed"
 		abort "$op"
 	fi
@@ -73,21 +52,18 @@ ui_print "* Setting Permissions"
 set_perm $MODPATH/base.apk 1000 1000 644 u:object_r:apk_data_file:s0
 
 ui_print "* Mounting __PKGNAME"
-mkdir $NVBASE/rvhc 2>/dev/null
-RVPATH=$NVBASE/rvhc/__PKGNAME_rv.apk
-mv -f $MODPATH/base.apk $RVPATH
+RVPATH=/data/adb/__PKGNAME_rv.apk
+ln -f $MODPATH/base.apk $RVPATH
 
-if ! op=$(su -Mc mount -o bind $RVPATH $BASEPATH/base.apk 2>&1); then
+if ! op=$(mount -o bind $RVPATH $BASEPATH 2>&1); then
 	ui_print "ERROR: Mount failed!"
-	ui_print "$op"
-	abort "Flash in official Magisk app"
+	abort "$op"
 fi
 am force-stop __PKGNAME
 ui_print "* Optimizing __PKGNAME"
 cmd package compile --reset __PKGNAME &
 
-ui_print "* Cleanup"
-rm -rf $MODPATH/bin $MODPATH/__PKGNAME.apk
+rm -r $MODPATH/bin $MODPATH/__PKGNAME.apk
 
 ui_print "* Done"
 ui_print "  by j-hc (github.com/j-hc)"
--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,13 +1,2 @@
 #!/system/bin/sh
-{
-	MODDIR=${0%/*}
-	MODULES=${MODDIR%/*}
-	NVBASE=${MODULES%/*}
-	rm $NVBASE/rvhc/__PKGNAME_rv.apk
-	rmdir $NVBASE/rvhc
-	if __ISBNDL :; then
-		until [ "$(getprop sys.boot_completed)" = 1 ]; do sleep 1; done
-		sleep 15
-		pm uninstall __PKGNAME
-	fi
-} &
+rm /data/adb/__PKGNAME_rv.apk

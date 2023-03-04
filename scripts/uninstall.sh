f221205b0d0e0669a39363ef5628cddb83be6a77
M Aswin Kishore <60577077+mak626@users.noreply.github.com>
2023-03-04 14:54:56 +0530

    Revert "revert: magisk scripts"
    This reverts commit 0b9f8ad598a125a0065442ad32b65913cf4541c0.

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,2 +1,13 @@
 #!/system/bin/sh
-rm /data/adb/__PKGNAME_rv.apk
+{
+	MODDIR=${0%/*}
+	MODULES=${MODDIR%/*}
+	NVBASE=${MODULES%/*}
+	rm $NVBASE/rvhc/__PKGNAME_rv.apk
+	rmdir $NVBASE/rvhc
+	if __ISBNDL :; then
+		until [ "$(getprop sys.boot_completed)" = 1 ]; do sleep 1; done
+		sleep 15
+		pm uninstall __PKGNAME
+	fi
+} &
